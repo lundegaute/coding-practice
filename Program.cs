b8671a5c3983e8f816462c83c8e866614a0eecdf
Gaute Lunde <lundegaute@gmail.com>
2025-10-25 18:15:55 +0200

    Kata: High

--- Program.cs
+++ Program.cs
@@ -2,10 +2,5 @@
 
 var kata6 = new Kata6();
 
-var tower = kata6.TowerBuilder(3);
-foreach(var floor in tower)
-{
-    Console.WriteLine(floor);
-}
-// check unicode for each
-// If unicode increases with more than 1, we know which one we are missing
+var res = kata6.High("aa b");
+Console.WriteLine(res);
--- obj/Debug/net9.0/coding-practice.AssemblyInfo.cs
+++ obj/Debug/net9.0/coding-practice.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("coding-practice")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0536890fb48f3f47a357764b475d3a02ced5491")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0db40a6b3fa4e4a5233a88d11e89f952361c02a")]
 [assembly: System.Reflection.AssemblyProductAttribute("coding-practice")]
 [assembly: System.Reflection.AssemblyTitleAttribute("coding-practice")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
