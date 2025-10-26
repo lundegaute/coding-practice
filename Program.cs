9d24e8e127f68821ad87add2c40601a40b12134e
Gaute Lunde <lundegaute@gmail.com>
2025-10-26 11:05:09 +0100

    Kata

--- Program.cs
+++ Program.cs
@@ -2,5 +2,8 @@
 
 var kata6 = new Kata6();
 
-var res = kata6.High("aa b");
-Console.WriteLine(res);
+var res = kata6.AllwaysStartWithABC(["ab", "bc", "cd", "ef", "ko"]);
+foreach(var word in res)
+{
+    Console.WriteLine(word);
+}
